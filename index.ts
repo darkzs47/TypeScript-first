4bf49faaff9ae47984b642afa7a007c0f4d4ec2a
Aleksandr <daaarkzs47@gmail.com>
2025-06-21 22:17:32 +0500

    Ready

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ interface User {
     name: string;
     age?: number;
     email?: string;
+    status?: Status
 }
 
 const Vasya: User = {
@@ -25,16 +26,22 @@ const Ivan: User = {
     name: "Ivan",
 }
 
-// 2
+// 2 and 5
+
+enum UserRole {
+    Admin,
+    User,
+    Guest,
+}
 
 interface Admin extends User {
-    role: string;
+    role: UserRole;
 }
 
 const Admin: Admin = {
     id: Date.now(),
     name: "Admin",
-    role: 'Admin',
+    role: UserRole.Admin,
 }
 
 // 3
@@ -44,5 +51,62 @@ const greetUser = (user: User): string => {
         return `Привет ${user.name}, тебе ${user.age} лет`;
     }
     return `Привет ${user.name}`
+}
+
+// 6
+
+const UserStatus = {
+    Active: "active",
+    Inactive: "inactive",
+    Pending: "pending",
+} as const;
+
+type UserStatusType = typeof UserStatus[keyof typeof UserStatus];
+
+// Доп
+
+type Status = "active" | "inactive" | 0 | 1
+let userStatus: Status = 1
+
+const getDescStatus = (status: Status) => {
+    switch (status) {
+        case "active":
+            return `Получен статус active: ${status}`;
+        case "inactive":
+            return `Получен статус inactive: ${status}`
+        case 0:
+            return `Получен статус 0: ${status}`
+        case 1:
+            return `Получен статус 1: ${status}`
+        default:
+            return `Неизвестный статус`
+    }
+}
+
+console.log(getDescStatus("active"))
+console.log(getDescStatus(userStatus))
+
+// 7 
+
+class Car {
+    make: string;
+    model: string;
+    year: number;
+    mileage: number = 0;
+
+    constructor(make: string, model: string, year: number) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    drive(distance: number): void {
+        this.mileage += distance;
+    }
+
+    getDescription(): string {
+        return `Машина: ${this.make}, модель: ${this.model}, год выпуска: ${this.year}, проехала: ${this.mileage} км.`;
+    }
+
 }
 
